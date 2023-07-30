6bb0b4733822b2ab76b2fe3b4c01748ec8139b5d
guywhohasagun <fergmacnz@gmail.com>
2023-07-31 10:00:52 +1200

    pump

--- Alarm_main/Alarm_main.ino
+++ Alarm_main/Alarm_main.ino
@@ -11,6 +11,9 @@ int alarmMinute = 0;
 int alarmHour = 0;
 int analogInput = 0;
 int buttonPin = 2;
+int pumpPin = 3;
+
+bool pumpOn = true;
  
 float timeHours = 0;
 int timeIntHours = 0;
@@ -28,6 +31,7 @@ void setup () {
     lcd.backlight();
     pinMode(analogPin, INPUT);
     pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(pumpPin, OUTPUT);
 }
 
 void MTOHM () {
@@ -54,9 +58,16 @@ void button () {
 
 void loop () {
   lcd.clear();
-  if (RTC.now().minute() == alarmMinute && RTC.now().hour() == alarmHour){
+  if (RTC.now().minute() == alarmMinute && RTC.now().hour() == alarmHour && pumpOn == true){
     Serial.println("Pump HIGH");
     lcd.println("pump high");
+    digitalWrite(pumpPin, HIGH);
+    delay(30000);
+  }
+  else {
+    digitalWrite(pumpPin, LOW);
+    Serial.println("pump low");
+    pumpOn = true;
   }
   lcd.println(String(timeIntHours) + ":" + String(timeMinutes));
 
@@ -66,9 +77,6 @@ void loop () {
 
   alarmMinute = EEPROM.read(1);
   alarmHour = EEPROM.read(0);
-
-  Serial.println(alarmMinute);
-  Serial.println(alarmHour);
 
   delay(100);
 }
